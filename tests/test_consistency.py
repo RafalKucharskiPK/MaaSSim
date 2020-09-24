6a2f7dbc989ca07e7a923af1e974c8cafcc9f9cd
Rafal <r.m.kucharski@tudelft.nl>
2020-09-24 10:59:29 +0200

    coverage

--- tests/test_consistency.py
+++ tests/test_consistency.py
@@ -5,25 +5,51 @@ __email__ = "r.m.kucharski@tudelft.nl"
 __license__ = "MIT"
 
 
-import unittest
-import os
 
+import unittest
+import os, sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..'))
+from MaaSSim.simulators import simulate
 CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config_consistency_test.json')
 
 
 class TestSimulationResults(unittest.TestCase):
 
+    def test_output(self):
+        self.sim = simulate(config=CONFIG_PATH, root_path=os.path.dirname(__file__))  # run simulations
+
+        self.assertIn('trips', self.sim.runs[0].keys())  # do we have the results
+
+        print(self.sim.sim_end - self.sim.sim_start)
+
+        self.assertLess(self.sim.sim_end - self.sim.sim_start, 20)  # does the calculation take a lot of time?
+
+        self.assertGreater(self.sim.runs[0].trips.pos.nunique(), self.sim.params.nP)  # do we travel around the city
+
+        self.assertGreater(self.sim.runs[0].trips.t.max(),
+                           0.5 * self.sim.params.simTime * 60 * 60) # do we span at least half of simulation time?)
+
+        paxes = self.sim.runs[0].rides.paxes.apply(lambda x: tuple(x))
+        self.assertGreater(paxes.nunique(), self.sim.params.nP/2)  # at least half of travellers got the ride
+
+        self.assertGreater(self.sim.res[0].veh_exp["ARRIVES_AT_PICKUP"].max(), 0)  # is there any vehicle RIDING?
+
+        self.assertIn('pax_exp', self.sim.res[0].keys())
+
+
+
     def test_consistency(self):
 
-        from MaaSSim.simulators import simulate
-        self.res = simulate(config=CONFIG_PATH, root_path=os.path.dirname(__file__))  # run simulations
+
+        self.sim = simulate(config=CONFIG_PATH, root_path=os.path.dirname(__file__))  # run simulations
 
         from MaaSSim.traveller import travellerEvent
 
-        rides = self.res.runs[0].rides  # vehicles results
-        trips = self.res.runs[0].trips  # travellers result
-        for i in self.res.inData.passengers.sample(min(5, self.res.params.nP)).index.to_list():
-            r = self.res.inData.requests[self.res.inData.requests.pax_id == i].iloc[0].squeeze()  # that is his request
+        rides = self.sim.runs[0].rides  # vehicles results
+        trips = self.sim.runs[0].trips  # travellers result
+        for i in self.sim.inData.passengers.sample(min(5, self.sim.params.nP)).index.to_list():
+            r = self.sim.inData.requests[self.sim.inData.requests.pax_id == i].iloc[0].squeeze()  # that is his request
             o, d = r['origin'], r['destination']  # his origin and destination
             trip = trips[trips.pax == i]  # his trip
             self.assertIn(o, trip.pos.values)  # was he at origin
