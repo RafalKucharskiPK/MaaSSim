e15a0b80cb0c71ed57ad9eec6980877914277d30
Rafal <r.m.kucharski@tudelft.nl>
2020-10-06 16:45:16 +0200

    batched platform test

--- tests/test_maassim.py
+++ tests/test_maassim.py
@@ -103,12 +103,6 @@ class TestSimulationResults(unittest.TestCase):
         self.assertTrue(sim2.runs[0].rides.equals(sim1.runs[0].rides))
 
 
-
-
-
-
-
-
     def test_parallel(self):
         from dotmap import DotMap
         search_space = DotMap()
@@ -117,6 +111,30 @@ class TestSimulationResults(unittest.TestCase):
         CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config_parallel_test.json')
 
         simulate_parallel(config = CONFIG_PATH, search_space=search_space, root_path=os.path.dirname(__file__))
+
+
+    def test_batch_platform(self):
+        from MaaSSim.utils import initialize_df
+
+        from MaaSSim.data_structures import structures as inData
+        from MaaSSim.utils import read_requests_csv, read_vehicle_positions
+        CONFIG_PATH = os.path.join(os.path.dirname(__file__), 'config_results_test.json')
+        params = get_config(CONFIG_PATH, root_path=os.path.dirname(__file__))  # load from .json file
+        inData = load_G(inData, params)  # load network graph
+        inData = prep_supply_and_demand(inData, params)  # generate supply and demand
+        inData.platforms = initialize_df(inData.platforms)
+        inData.platforms.loc[0] = [1, 'Uber', 600]  # batch requests every 600 seconds
+
+        sim = simulate(params=params, inData=inData, event_based=False)
+        Qs = sim.runs[0].queues
+        Qs.groupby('platform')[['vehQ', 'reqQ']].plot(drawstyle='steps-post')
+
+        r = sim.runs[0].trips
+        times = r[r.event == 'RECEIVES_OFFER'].t.sort_values(ascending=True).diff().dropna().unique()
+
+        self.assertEqual(len(times), 2) # are requests batched ony at batch_time
+        self.assertEqual(times[-1],inData.platforms.iloc[0].batch_time)  # are they batched at batch_time
+
 
     def tearDown(self):
         zips = glob.glob('*.{}'.format('zip'))
