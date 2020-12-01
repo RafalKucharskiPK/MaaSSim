e44ee9e99ab8ce1cb4d4f5bffb3da27793961c0d
aderuijter <a.j.f.deruijter@tudelft.nl>
2020-12-01 13:38:18 +0100

    created compatible generate_vehicles_d2d function

--- MaaSSim/d2d.py
+++ MaaSSim/d2d.py
@@ -19,7 +19,7 @@ def D2D_veh_exp(*args,**kwargs):
     df=df[~(df.t == df.t_s)] # filter for positive time periods only
     df['dt'] = df.t_s - df.t # make time intervals
     ret = df.groupby(['veh','event'])['dt'].sum().unstack() #aggregated by vehicle and event
-    del ret.columns.name 
+    del ret.columns.name
     ret = ret.reindex(vehindex) #update for vehicles with no record
     ret['nRIDES'] = df[df.event == driverEvent.ARRIVES_AT_DROPOFF.name].groupby(
         ['veh']).size().reindex(ret.index)
@@ -30,7 +30,7 @@ def D2D_veh_exp(*args,**kwargs):
             ret[status.name]=0 #cover all statuss
     DECIDES_NOT_TO_DRIVE.index = DECIDES_NOT_TO_DRIVE.values
     ret['OUT'] = DECIDES_NOT_TO_DRIVE
-    ret['OUT'] = ~ret['OUT'].isnull()   
+    ret['OUT'] = ~ret['OUT'].isnull()
     ret['DRIVING_TIME'] = ret.REJECTS_REQUEST + ret.IS_REJECTED_BY_TRAVELLER + ret.IS_ACCEPTED_BY_TRAVELLER + ret.DEPARTS_FROM_PICKUP + ret.CONTINUES_SHIFT
     ret['DRIVING_DIST'] = ret['DRIVING_TIME'] * (params.speeds.ride/1000)
     ret['REVENUE'] = (ret.DEPARTS_FROM_PICKUP * (params.speeds.ride/1000) * params.fares.km).add(params.fares.base * ret.nRIDES) * (1-params.fares.commission)
@@ -38,7 +38,7 @@ def D2D_veh_exp(*args,**kwargs):
     ret['NET_INCOME'] = ret['REVENUE'] - ret['COST']
     ret = ret[['nRIDES','nREJECTED', 'DRIVING_TIME', 'DRIVING_DIST', 'REVENUE', 'COST', 'NET_INCOME', 'OUT']+[_.name for _ in driverEvent]].fillna(0) #nans become 0
     ret.index.name = 'veh'
-    
+
     #KPIs
     kpi = ret.agg(['sum','mean','std'])
     kpi['nV']=ret.shape[0]
@@ -47,7 +47,7 @@ def D2D_veh_exp(*args,**kwargs):
 def D2D_driver_out(*args, **kwargs):
     """ returns True if driver decides not to drive, and False if he drives"""
     veh = kwargs.get('veh',None)
-    
+
     perc_income = veh.veh.expected_income
     if ~veh.veh.registered:
         return True
@@ -56,12 +56,12 @@ def D2D_driver_out(*args, **kwargs):
         util_nd = veh.sim.params.drivers.particip.beta * veh.veh.res_wage
         prob_d_reg = math.exp(util_d) / (math.exp(util_d) + math.exp(util_nd))
         prob_d_all = prob_d_reg
-        
+
         if prob_d_all < random.random():
             #print('I go out because I expect to earn too litle today')
             return True
         else:
-            return False  
+            return False
     else:
         if perc_income < veh.veh.res_wage:
             #print('I go out because I expect to earn too litle today')
@@ -77,7 +77,7 @@ def update_d2d_exp(*args, **kwargs):
 #     ret = dict()
     hist = pd.concat([~sim.res[_]['veh_exp'].OUT for _ in range(0,run_id+1)],axis=1,ignore_index=True)
     worked_days = hist.sum(axis=1)
-    
+
     ret = pd.DataFrame()
     ret['veh'] = np.arange(1,params.nV+1)
     ret['informed'] = sim.vehicles.informed.to_numpy()
@@ -93,14 +93,14 @@ def update_d2d_exp(*args, **kwargs):
     ret['new_perc_inc'] = new_perc_inc.to_numpy()
     ret.loc[(ret.registered == True) & (ret.out == True), 'new_perc_inc'] = ret.loc[(ret.registered == True) & (ret.out == True), 'init_perc_inc']
     ret = ret.set_index('veh')
-    
+
     return  ret
 
 def D2D_stop_crit(*args, **kwargs):
     "returns True if simulation will be stopped, False otherwise"
     res = kwargs.get('d2d_res', None)
     params = kwargs.get('params', None)
-    
+
     if len(res) < params.min_it:
         return False
     else:
@@ -115,19 +115,19 @@ def platform_regist(inData, end_day, **kwargs):
     params = kwargs.get('params', None)
     exp_reg_drivers = end_day.loc[end_day['registered'] == True]
     average_perc_income = exp_reg_drivers.new_perc_inc.mean()
-    
+
     samp = np.random.rand(params.nV) <= params.drivers.regist.samp   # Sample of drivers making registration choice
-    
+
     util_reg = params.drivers.regist.beta * average_perc_income
     util_not_reg = params.drivers.regist.beta * (inData.vehicles.res_wage + params.drivers.regist.cost_comp)
     prob_regist = np.exp(util_reg) / (np.exp(util_reg) + np.exp(util_not_reg))
     regist_decision = (np.random.rand(params.nV) < prob_regist) & inData.vehicles.informed & samp
-    
+
     prev_regist = inData.vehicles.registered.to_numpy()
     registered = (np.concatenate(([prev_regist],[regist_decision]),axis=0).transpose()).any(axis=1)
     regist_res = pd.DataFrame(data = {'registered': registered, 'expected_income': end_day.new_perc_inc})
     regist_res.loc[((inData.vehicles.registered == False) & (regist_res.registered == True)), "expected_income"] = average_perc_income
-    
+
     return regist_res
 
 def word_of_mouth(inData, **kwargs):
@@ -140,12 +140,12 @@ def word_of_mouth(inData, **kwargs):
         prob_inf = exp_inf_day / nV_uninf
     else:
         prob_inf = 0
-    
+
     new_inf = np.random.rand(params.nV) < prob_inf
     prev_inf = inData.vehicles.informed.to_numpy()
     informed = (np.concatenate(([prev_inf],[new_inf]),axis=0).transpose()).any(axis=1)
     res_inf = pd.DataFrame(data = {'informed': informed}, index=np.arange(1,params.nV+1))
-    
+
     return res_inf
 
 def D2D_summary(d2d):
@@ -168,6 +168,34 @@ def D2D_summary(d2d):
     evol_micro.exp_inc = exp_inc
     evol_stats = pd.DataFrame({'inform': evol_micro.inform.sum(), 'regist': evol_micro.regist.sum(), 'particip': evol_micro.ptcp.sum(), 'mean_perc_inc': evol_micro.perc_inc.mean(), 'mean_exp_inc': evol_micro.exp_inc.mean()})
     evol_stats.index.name = 'day'
-    
+
     return evol_micro, evol_stats
-        
+
+def generate_vehicles_d2d(_inData, _params=None):
+    """
+    generates vehicle database
+    index is consecutive number if dataframe
+    registered whether drivers have already made sign up decision
+    position is random graph node
+    event is set to STARTS_DAY
+    """
+
+    vehs = generate_vehicles(_inData, _params.nV)
+
+    # vehs = list()
+    # for i in range(_params.nV + 1):
+    #     vehs.append(empty_series(inData_.vehicles, id=i))
+    #
+    # vehs = pd.concat(vehs, axis=1, keys=range(1, _params.nV + 1)).T
+    # vehs.event = driverEvent.STARTS_DAY
+    vehs.expected_income = float("NaN")
+    vehs['res_wage'] = np.random.normal(_params.drivers.res_wage.mean, _params.drivers.res_wage.std, _params.nV)
+    vehs['informed'] = (np.random.rand(_params.nV) < _params.drivers.inform.prob_start)
+    vehs['registered'] = (np.random.rand(_params.nV) < _params.drivers.regist.prob_start) & vehs.informed
+    vehs.loc[vehs.registered == True, "expected_income"] = _params.drivers.init_income
+    # vehs.platform = 0
+    # vehs.shift_start = 0
+    # vehs.shift_end = 60 * 60 * 24
+    # vehs.pos = vehs.pos.apply(lambda x: int(rand_node(inData_.nodes)))
+
+    return vehs
