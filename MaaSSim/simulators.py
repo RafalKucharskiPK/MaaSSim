137f2aab0012dae8d7d3cb7d3f75e2883324d6f0
aderuijter <a.j.f.deruijter@tudelft.nl>
2020-12-08 16:45:41 +0100

    changed simulators.py to account for new fare elements

--- MaaSSim/simulators.py
+++ MaaSSim/simulators.py
@@ -61,7 +61,10 @@ def simulate_parallel(config="../data/config/parallel.json", inData=None, params
     if len(inData.platforms) == 0:  # only if no platforms in input
         inData.platforms = initialize_df(inData.platforms)
         inData.platforms.loc[0] = empty_series(inData.platforms)
-        inData.platforms.fare = [1]
+        inData.platforms.km_fare = [1]
+        inData.platforms.base_fare = [0]
+        inData.platforms.comm_rate = [0.25]
+        inData.platforms.min_fare = [0]
         inData.vehicles.platform = 0
         inData.passengers.platforms = inData.passengers.apply(lambda x: [0], axis=1)
 
