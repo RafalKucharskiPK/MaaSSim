cb88c37fc757222c14eb49f9898731e6fd9db770
usmanakhtar <usman@oslab.khu.ac.kr>
2023-01-17 12:13:06 +0100

    MY choice function for max. revenue ride

--- MaaSSim/pool_price.py
+++ MaaSSim/pool_price.py
@@ -11,12 +11,12 @@ def pool_price_fun(sim, veh, request):
     logger = sim.logger.critical # set what do you wantto see from the logger
     
     if len(request.rides)>1: # only if there is a choice
-        logger('this is reuqest {} with {} available rides.'.format(request.pax_id, request.rides))
+        logger('this is request {} with {} available rides.'.format(request.pax_id, request.rides))
         available_rides = sim.inData.sblts.rides.loc[request.rides] # this is set in shared.py
         still_available_rides = list()
         for ride_index, ride in available_rides.iterrows():
             if all([sim.pax[_].pax.event.value <= 1 for _ in ride.indexes]):
-                still_available_rides.append(ride_index) # see all the tavellers of this pooled ride are still available
+                still_available_rides.append(ride_index) # see all the travellers of this pooled ride are still available
                 logger("ride {} available {}".format(ride.name, [sim.pax[_].pax.event.value for _ in ride.indexes])) 
             else:
                 logger("ride {} not available {}".format(ride.name, [sim.pax[_].pax.event.value for _ in ride.indexes]))
@@ -28,7 +28,9 @@ def pool_price_fun(sim, veh, request):
 
             #### HERE COMES YOUR CHOICE FUNCTIONS
 
-            my_choice = still_available_rides.sample(1).squeeze() # random choice - to be overwritten with different func
+            #my_choice = still_available_rides.sample(1).squeeze() # random choice - to be overwritten with different func
+            my_choice = still_available_rides[still_available_rides["driver_revenue"]==still_available_rides["driver_revenue"].max()].squeeze()
+            #print(my_choice)
 
             logger('vehicle {} has chosen to serve request {} with a ride {} of degree {}, with travellers {}.'.format(veh.id, request.pax_id, my_choice.name, my_choice.degree, my_choice.indexes ))
 
@@ -36,7 +38,9 @@ def pool_price_fun(sim, veh, request):
             for pax in my_choice.indexes:
                 sim.inData.requests.loc[pax].sim_schedule = my_choice.sim_schedule
                 # maybe here we need to update the position and leader of the schedule - and set that the schedule got triggered? - so far no bugs
+    
     return request, sim
+
     
 
 
